de469a3f005b0a32344f858ffcb49326498ddc63
Ruan <79723457+ruann3res@users.noreply.github.com>
2022-03-30 19:24:11 -0300

    Create Ex05.java

---
+++ Ex05.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int codigo = sc.nextInt();
+	    int quantidade = sc.nextInt();
+	    
+	   double total;
+	   
+	   if (codigo == 1);{
+	       total = quantidade * 12.00 ;
+	   }
+	   if (codigo == 2);{
+	       total = quantidade * 17.00 ;
+	   }
+	   if (codigo == 3);{
+	       total = quantidade * 25.00 ;
+	   }
+	    System.out.println(total);
+	}
+}
